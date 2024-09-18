87cc8948b8a7147bc1aaa269dd0af3181407bf11
Xiaoxi He <xxhe@alauda.io>
2024-09-18 10:31:38 +0800

    Support basic auth

--- lua/cmp_jira/source.lua
+++ lua/cmp_jira/source.lua
@@ -1,21 +1,17 @@
 local cmp = require("cmp")
-local curl = require "plenary.curl"
+local curl = require("plenary.curl")
 local utils = require("cmp_jira.utils")
 
 local source = {
   config = {},
   filetypes = {},
-  cache = {}
+  cache = {},
 }
 
 source.new = function(overrides)
-  local self =
-      setmetatable(
-        {},
-        {
-          __index = source
-        }
-      )
+  local self = setmetatable({}, {
+    __index = source,
+  })
 
   self.config = vim.tbl_extend("force", require("cmp_jira.config"), overrides or {})
   for _, item in ipairs(self.config.filetypes) do
@@ -24,7 +20,8 @@ source.new = function(overrides)
 
   -- defaults
   if self.config.jira.jql == nil or self.config.jira.jql == "" then
-    self.config.jira.jql = "(assignee = currentUser() OR reporter = currentUser()) order by updated DESC"
+    self.config.jira.jql =
+      "(assignee = currentUser() OR reporter = currentUser()) order by updated DESC"
   end
 
   return self
@@ -45,40 +42,35 @@ function source:complete(_, callback)
   local req_url = utils.get_request_url(self.config)
 
   -- run curl command
-  curl.get(
-    req_url,
-    {
-      headers = {
-        Authorization = utils.get_auth_header()
-      },
-      callback = function(out)
-        local ok, parsed_issues = utils.parse_api_response(out.body)
-        if not ok then
-          return false
-        end
-        print(vim.inspect(parsed_issues))
-
-        local items = {}
-        for _, issue in ipairs(parsed_issues) do
-          table.insert(
-            items,
-            {
-              label = string.format("%s: %s", issue.key, issue.summary),
-              filterText = string.format("%s: %s", issue.key, issue.summary),
-              insertText = issue.key,
-              sortText = issue.key
-            }
-          )
-        end
-
-        -- update the cache
-        self.cache[bufnr] = items
-
-        callback({ items = items })
-        return true
+  curl.get(req_url, {
+    -- headers = {
+    --   Authorization = utils.get_auth_header()
+    -- },
+    auth = utils.get_basic_auth(self.config),
+    callback = function(out)
+      local ok, parsed_issues = utils.parse_api_response(out.body)
+      if not ok then
+        return false
       end
-    }
-  )
+      print(vim.inspect(parsed_issues))
+
+      local items = {}
+      for _, issue in ipairs(parsed_issues) do
+        table.insert(items, {
+          label = string.format("%s: %s", issue.key, issue.summary),
+          filterText = string.format("%s: %s", issue.key, issue.summary),
+          insertText = issue.key,
+          sortText = issue.key,
+        })
+      end
+
+      -- update the cache
+      self.cache[bufnr] = items
+
+      callback({ items = items })
+      return true
+    end,
+  })
 
   return false
 end
--- lua/cmp_jira/utils.lua
+++ lua/cmp_jira/utils.lua
@@ -1,79 +1,78 @@
 local M = {}
 
 M.parse_api_response = function(response)
-    local ok, parsed = pcall(vim.json.decode, response)
-    if not ok then
-        return false, {}
+  local ok, parsed = pcall(vim.json.decode, response)
+  if not ok then
+    return false, {}
+  end
+
+  if not parsed.issues then
+    return false, {}
+  end
+
+  local issues = {}
+  for _, issue in ipairs(parsed.issues) do
+    if not issue.key then
+      return false, {}
     end
 
-    if not parsed.issues then
-        return false, {}
+    local summary_val = ""
+    if issue.fields then
+      if issue.fields.summary then
+        summary_val = issue.fields.summary
+      end
     end
 
-    local issues = {}
-    for _, issue in ipairs(parsed.issues) do
+    table.insert(issues, {
+      key = issue.key,
+      summary = summary_val,
+    })
+  end
 
-        if not issue.key then
-            return false, {}
-        end
-
-        local summary_val = ''
-        if issue.fields then
-            if issue.fields.summary then
-              summary_val = issue.fields.summary
-            end
-        end
-
-       table.insert(issues, {
-           key = issue.key,
-           summary = summary_val,
-       })
-    end
-
-    return true, issues
+  return true, issues
 end
 
 M.get_basic_auth = function(config)
-    local user = M.get_user(config)
-    local api_key = vim.fn.getenv("JIRA_USER_API_KEY")
-    return user .. ':' .. api_key
+  local user = M.get_user(config)
+  local api_key = vim.fn.getenv("JIRA_USER_API_KEY")
+  return user .. ":" .. api_key
 end
 
 M.get_auth_header = function(config)
-    local api_key = vim.fn.getenv("JIRA_USER_API_KEY")
-    return string.format("Bearer %s", api_key)
+  local api_key = vim.fn.getenv("JIRA_USER_API_KEY")
+  return string.format("Bearer %s", api_key)
 end
 
 M.get_request_url = function(config)
-    local url = M.get_jira_url(config)
-    local jql = M.get_jql(config)
-    return string.format('%s/rest/api/2/search?fields=summary&jql=', url) ..  jql
+  local url = M.get_jira_url(config)
+  local jql = M.get_jql(config)
+  return string.format("%s/rest/api/2/search?fields=summary&jql=", url) .. jql
 end
 
 M.get_jql = function(config)
-    local username = M.get_username(config)
-    return string.format(config.jira.jql, username)
+  local username = M.get_username(config)
+  return string.format(config.jira.jql, username)
 end
 
 M.get_jira_url = function(config)
-    local url = config.jira.url
-    if vim.fn.exists("$JIRA_WORKSPACE_URL") == 1 then
-        url = vim.fn.getenv("JIRA_WORKSPACE_URL")
-    end
-    return url
+  local url = config.jira.url
+  if vim.fn.exists("$JIRA_WORKSPACE_URL") == 1 then
+    url = vim.fn.getenv("JIRA_WORKSPACE_URL")
+  end
+  return url
 end
 
 M.get_user = function(config)
-    local user = config.jira.email
-    if vim.fn.exists("$JIRA_USER_EMAIL") == 1 then
-        user = vim.fn.getenv("JIRA_USER_EMAIL")
-    end
-    return user
+  local user = config.jira.email
+  if vim.fn.exists("$JIRA_USER_EMAIL") == 1 then
+    user = vim.fn.getenv("JIRA_USER_EMAIL")
+  end
+  return user
 end
 
 M.get_username = function(config)
-    local user = M.get_user(config)
-    return string.gsub(user, "@", "\\u0040")
+  local user = M.get_user(config)
+  return string.gsub(user, "@", "\\u0040")
 end
 
 return M
